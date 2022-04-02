b10495d528aa7c9926db93372e33709f5e34075c
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-04-02 17:59:53 +0800

    Update

---
+++ main.js
@@ -0,0 +1,3 @@
+document.getElementById("catagory-box-recommend-x").onclick = () => {
+  document.querySelector(".main-container").removeChild(document.querySelector(".row-catagory-box-recommend"));
+}
